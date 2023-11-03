35e6714980a85eab4eb17df68cff97c195d126de
Jucy Abuda Neto <140865218+Netozo@users.noreply.github.com>
2023-11-03 11:24:15 -0300

    Comentarios em cada linha

--- BGInfo/BGInfo.bat
+++ BGInfo/BGInfo.bat
@@ -1,15 +1,21 @@
 @echo off
 setlocal
 
-rem Define o caminho da pasta compartilhada
-set "caminho_compartilhado=\\lucida\papel$"
+rem Define o caminho da pasta compartilhada onde estão os arquivos
+set "caminho_compartilhado=\\servidor\pastacompartilhada"
 
 rem Define o caminho da pasta local
 set "caminho_local=C:\bginfo"
 
 rem Define os nomes dos arquivos a serem copiados
+rem Bginfo64.exe é o executavel do bginfo
+
 set "arquivo1=Bginfo64.exe"
-set "arquivo2=bgljd.bgi"
+
+rem bginfo.bgi é o executavel que voce salva apartir do .exe com suas configuraçoes
+set "arquivo2=bginfo.bgi"
+
+rem Aqui é o arquivo .jpg que sera o papel de parede
 set "arquivo3=papel.jpg"
 
 rem Verifica se a pasta local existe, e se não, cria-a
@@ -33,6 +39,7 @@ for /f "tokens=1-2 delims=: " %%a in ('time /t') do (
 rem Escreve uma mensagem de conclusão no arquivo de log com data e hora
 echo Log (%data% - %hora%): Arquivos copiados com sucesso! >> C:\bginfo\log.txt
 
+rem Após copiar e gravar log ele executa o bginfo na estação no modo silencioso
 start "" "%caminho_local%\%arquivo1%" /timer:0 /silent /nolicprompt "%caminho_local%\%arquivo2%"
 
 endlocal
